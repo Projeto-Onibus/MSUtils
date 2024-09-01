986553eebe68011d707cd389be42667e45db91af
Fernando Dias <fernando.dias.3741@gmail.com>
2024-09-01 17:10:20 -0300

    Added server exception to exception list

--- microservice_utils/AppException.py
+++ microservice_utils/AppException.py
@@ -9,3 +9,8 @@ class ClientException(AppException):
     def __init__(self,message):
         self.statusCode=400
         self.message = message
+
+class ServerException(AppException):
+    def __init__(self,message):
+        self.statusCode=500
+        self.message = message
